425f5b1ab70b4d1d021d2dd6f4456354e90e8b0e
Ubuntu <ubuntu@ip-172-31-17-11.us-east-2.compute.internal>
2018-06-02 14:26:01 +0000

    0602

--- truffle.js
+++ truffle.js
@@ -3,9 +3,16 @@ module.exports = {
   // for more about customizing your Truffle configuration!
   networks: {
     development: {
-      host: "127.0.0.1",
-      port: 7545,
+      host: "localhost",
+      port: 8545,
       network_id: "*" // Match any network id
+    },
+    rinkeby: {
+      host: "localhost", // Connect to geth on the specified
+      port: 8545,
+      from: "0x858FE233e74b464627F958e03e222d14aF8f4841", // default address to use for any transaction Truffle makes during migrations
+      network_id: 4,
+      gas: 4612388 // Gas limit used for deploys
     }
   }
 };
